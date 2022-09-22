e043de77704bcf8c6eaba71923d6e1763335f2d9
Shohboz Rejabaliyev <rejabaliyevshohbozbek2006@gmail.com>
2022-09-22 17:18:42 +0500

    Day 2 Selenium. HomeWorks

---
+++ src/main/tests/selenium_homeworks/day2/Exs_3.java
@@ -0,0 +1,36 @@
+package selenium_homeworks.day2;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Exs_3 {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://google.com");
+
+        WebElement searchGMAIL = driver.findElement(By.className("gb_d"));
+        searchGMAIL.sendKeys("Gmail" + Keys.ENTER);
+
+        String expectedTitle = "Gmail";
+        String actualTitle = driver.getTitle();
+
+        System.out.println(actualTitle.contains(expectedTitle) ? "Title verification is PASSED" : "Title verification is FAILED");
+
+        driver.navigate().back();
+
+        String expectedName = "Google";
+        String actualName = driver.getTitle();
+
+        System.out.println(actualName.equals(expectedName) ? "PASSED" : "FAILED");
+
+
+      //  driver.quit();
+    }
+}
