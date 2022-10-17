d94947b6471abddfdd4d44a2ab0e003e5331a012
Shohboz Rejabaliyev <rejabaliyevshohbozbek2006@gmail.com>
2022-10-17 19:09:34 +0500

    Day 10 Selenium

---
+++ src/test/java/com/cydeo/tests/selenium_practice/cydeo/practice_cydeo_statusCodes2.java
@@ -0,0 +1,10 @@
+package com.cydeo.tests.selenium_practice.cydeo;
+
+import com.cydeo.tests.test.TestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class practice_cydeo_statusCodes2 extends TestBase {
+
+}
